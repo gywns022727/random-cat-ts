ccbaad457d7075b51ae56b98daad13236fcc602b
gywns022727 <lifford050227a@gmail.com>
2023-06-01 15:53:27 +0900

    update

--- src/pages/Cat/index.tsx
+++ src/pages/Cat/index.tsx
@@ -9,19 +9,24 @@ import Button from "../../components/Button";
 Modal.setAppElement("#root");
 
 export default function Index() {
-  const [cats, setCats] = useState<GetCatsResponse[]>([]);
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [cats, setCats] = useState<GetCatsResponse[]>([]); // 고양이 사진 data들을 담아놓을 state
+  const [isOpen, setIsOpen] = useState<boolean>(false); // Modal의 상태를 담아놓은 state
 
-  const { data, isFetching, refetch } = useQuery(["cats"], getCats);
+  const { data, isFetching, refetch } = useQuery(["cats"], getCats); // react-query를 사용하여 고양이 사진 가져오는 api를 실행함
 
-  const refresh = () => refetch();
+  // data: api로 불러온 response data
+  // isFetching: api요청으로 fetch가 일어나는 상황을 알려줌
+  // refetch: api 재요청
 
-  const handleOpenModal = () => setIsOpen(true);
-  const handleCloseModal = () => setIsOpen(false);
+  const refresh = () => refetch(); // react-query의 refetch 함수를 실행시키는 함수
 
+  const handleOpenModal = () => setIsOpen(true); // Modal의 상태를 true(보여줌)으로 바꿔주는 함수
+  const handleCloseModal = () => setIsOpen(false); // Modal의 상태를 false(숨김)으로 바꿔주는 함수
+
+  // 위에서 쓰는 data가 변경 될 때마다 밑의 함수가 실행됨
   useEffect(() => {
-    if (!data) return;
-    setCats(data);
+    if (!data) return; // data가 undefined 일 수 있어서 if문으로 걸러줌
+    setCats(data); // data가 값이 온전히 있으면 cats에 담아줌
     // console.log({ data });
   }, [data]);
 
@@ -43,7 +48,11 @@ export default function Index() {
           })}
         </List>
       )}
-      <Modal isOpen={isOpen} onRequestClose={handleCloseModal}></Modal>
+      <Modal
+        isOpen={isOpen} // Modal이 열려있는 상태인가
+        shouldCloseOnOverlayClick={true} // Modal의 바깥을 클릭하면 Modal이 꺼지는가
+        onRequestClose={handleCloseModal} // Modal을 끄는 요청을 하면 실행되는 함수
+      ></Modal>
     </Container>
   );
 }
